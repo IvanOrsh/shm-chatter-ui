8340e66e5e1743218d0229d56f9086b87ee47f88
IvanOrsh <ksironov@mail.ru>
2024-01-24 19:26:10 +0300

    feat: update login/signup

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import { Link as MUILink } from "@mui/material";
+
 import { Auth } from "./Auth";
 
 export function Login() {
   return (
     <Auth submitLabel="Log in" onSubmit={async () => {}}>
-      <Link to={"/signup"}>Don't have an account?</Link>
+      <Link to={"/signup"} style={{ alignSelf: "center" }}>
+        <MUILink>Don't have an account?</MUILink>
+      </Link>
     </Auth>
   );
 }
--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
+import { Link as MUILink } from "@mui/material";
 
 import { Auth } from "./Auth";
 
 export function SignUp() {
   return (
     <Auth submitLabel="Sign Up" onSubmit={async () => {}}>
-      <Link to={"/login"}>Already have an account?</Link>
+      <Link to={"/login"} style={{ alignSelf: "center" }}>
+        <MUILink>Already have an account?</MUILink>
+      </Link>
     </Auth>
   );
 }
