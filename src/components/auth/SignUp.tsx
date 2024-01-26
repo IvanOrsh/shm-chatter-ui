786dbd706b5f2ca12f3a263ff38ed785532cffa1
IvanOrsh <ksironov@mail.ru>
2024-01-27 01:37:45 +0300

    feat: implement signing up

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -2,10 +2,30 @@ import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
 
 import { Auth } from "./Auth";
+import { useCreateUser } from "../../hooks/useCreateUser";
 
 export function SignUp() {
+  const [createUser] = useCreateUser();
+
+  const onSubmit = async ({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }) => {
+    await createUser({
+      variables: {
+        createUserInput: {
+          email,
+          password,
+        },
+      },
+    });
+  };
+
   return (
-    <Auth submitLabel="Sign Up" onSubmit={async () => {}}>
+    <Auth submitLabel="Sign Up" onSubmit={onSubmit}>
       <Link to={"/login"} style={{ alignSelf: "center" }}>
         <Button variant="text" color="secondary">
           Already have an account?
