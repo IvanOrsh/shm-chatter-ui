c834c7cc7b4df4997011e4620fbf602418e5fc78
IvanOrsh <ksironov@mail.ru>
2024-01-27 00:49:41 +0300

    feat: implement apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// TODO: extract apollo provider
 import {
   Container,
   CssBaseline,
@@ -5,8 +6,10 @@ import {
   createTheme,
 } from "@mui/material";
 import { RouterProvider } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
 
 import { router } from "./components/Routes";
+import client from "./constants/apollo-client";
 
 const darkTheme = createTheme({
   palette: {
@@ -16,12 +19,14 @@ const darkTheme = createTheme({
 
 function App() {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Container>
-        <RouterProvider router={router} />
-      </Container>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Container>
+          <RouterProvider router={router} />
+        </Container>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
