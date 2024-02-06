c36637482a886c21f890e376b0fe00a20ba35df8
IvanOrsh <ksironov@mail.ru>
2024-02-07 00:43:20 +0300

    fix: type

--- codegen.ts
+++ codegen.ts
@@ -1,16 +1,15 @@
-
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://localhost:3000/qraphql",
+  schema: "http://localhost:3000/graphql",
   documents: "src/**/*.ts",
   generates: {
-    "src/gql": {
+    "src/gql/": {
       preset: "client",
-      plugins: []
-    }
-  }
+      plugins: [],
+    },
+  },
 };
 
 export default config;
